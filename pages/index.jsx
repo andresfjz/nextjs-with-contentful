a2d0565f85478248e2a7388fbe63ee0bb34853f1
Andrés Jiménez <andresfjz@hotmail.com>
2022-04-16 21:27:01 -0500

    Retrieving recipes information from Contentful

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,22 @@
-export default function Index() {
+import { createClient } from "contentful";
+
+export default function Index({ recipes }) {
+  console.log(recipes);
+  
   return <div className="recipeList">Recipe List</div>;
+}
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_SPACE_ACCESS_TOKEN,
+  });
+
+  const response = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: response.items
+    }
+  }
 }
